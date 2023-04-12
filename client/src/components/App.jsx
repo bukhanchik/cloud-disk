c5e97a98c3fcaef6d7a8129f1b0646d1615cb200
buka <bukeyev.alikhan@gmail.com>
2023-04-12 12:11:03 +0600

    lsn10_getFiles

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Registration from "./authorization/Registration";
 import Login from "./authorization/Login";
 import {useDispatch, useSelector} from "react-redux";
 import {auth} from "../actions/user";
+import Disk from "./disk/Disk";
 
 function App() {
     const isAuth = useSelector(state => state.user.isAuth)
@@ -21,10 +22,16 @@ function App() {
             <div className='app'>
                 <Navbar/>
                 <div className="wrap">
-                    {!isAuth &&
+                    {!isAuth ?
                         <Routes>
                             <Route path="/registration" element={<Registration/>}/>
                             <Route path="/login" element={<Login/>}/>
+
+                        </Routes>
+                        :
+                        <Routes>
+                            <Route exact path="/" element={<Disk/>}/>
+
                         </Routes>
                     }
                 </div>
